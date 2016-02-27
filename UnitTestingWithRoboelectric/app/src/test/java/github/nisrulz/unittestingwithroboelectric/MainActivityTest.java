8d004c55982cb6806ea2e9db7666b197a08e5438
Nishant Srivastava <nisrulz@gmail.com>
2016-02-28 03:15:43 +0530

    (+) project : UnitTestingWithRoboelectric

---
+++ UnitTestingWithRoboelectric/app/src/test/java/github/nisrulz/unittestingwithroboelectric/MainActivityTest.java
@@ -0,0 +1,39 @@
+package github.nisrulz.unittestingwithroboelectric;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+
+    @Test
+    public void validateTextViewHasText(){
+        TextView tvHelloWorld = (TextView) activity.findViewById(R.id.tvHelloWorld);
+        assertNotNull("TextView could not be found", tvHelloWorld);
+
+        assertTrue("TextView contains incorrect text",
+                "Hello World!".equals(tvHelloWorld.getText().toString()));
+
+
+    }
+
+}
