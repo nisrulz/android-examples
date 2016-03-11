33a0e9b8be2f4367e5db0abc6f68e932d47810ce
Nishant Srivastava <nisrulz@gmail.com>
2016-03-11 15:14:38 +0530

    (*) basic implementation of recycler view

--- RecyclerView/app/src/main/java/github/nisrulz/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/github/nisrulz/recyclerview/MainActivity.java
@@ -1,13 +1,50 @@
 package github.nisrulz.recyclerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    RecyclerView rv;
+    ArrayList<String> data;
+    RVAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = (RecyclerView) findViewById(R.id.rv_fruits);
+
+        data = new ArrayList<>();
+        data.add("Apple");
+        data.add("Banana");
+        data.add("Peach");
+        data.add("Pineapple");
+        data.add("Orange");
+        data.add("Strawberry");
+        data.add("Grapes");
+        data.add("Apricot");
+        data.add("Avocado");
+        data.add("Raisin");
+        data.add("Guava");
+        data.add("Papaya");
+        data.add("Pear");
+        data.add("Blueberry");
+        data.add("Lychee");
+        data.add("Date");
+        data.add("Fig");
+
+        adapter = new RVAdapter(data);
+        rv.hasFixedSize();
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
+
+
     }
 }
---
+++ RecyclerView/app/src/main/java/github/nisrulz/recyclerview/RVAdapter.java
@@ -0,0 +1,47 @@
+package github.nisrulz.recyclerview;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ItemViewHolder> {
+
+    List<String> dataList;
+
+    public RVAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list_item, parent,
+                false);
+
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.txt.setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView txt;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            txt = (TextView) itemView.findViewById(R.id.txt);
+        }
+    }
+}
