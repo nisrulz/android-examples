881095fed4596db922f68bb39b89b5b612f20bab
Nishant Srivastava <nisrulz@gmail.com>
2016-03-30 17:54:42 +0530

    (*) project : ListView  - Implemented Listview

--- ListView/app/src/main/java/github/nisrulz/listview/MainActivity.java
+++ ListView/app/src/main/java/github/nisrulz/listview/MainActivity.java
@@ -1,13 +1,35 @@
 package github.nisrulz.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayList<String> data;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.listview_1);
+        data = new ArrayList<>();
+        data.add("Apple");
+        data.add("Peach");
+        data.add("Pineapple");
+        data.add("Banana");
+        data.add("Orange");
+        data.add("Watermelon");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
+
+
     }
 }
