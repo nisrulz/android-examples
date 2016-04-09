ec89814151920cd1831171b9d71b6716c0f51984
Nishant Srivastava <nisrulz@gmail.com>
2016-04-10 01:01:37 +0530

    (*) implemented ParallaxHeaderListview

--- ParallaxHeaderListview/app/src/main/java/github/nisrulz/parallaxheaderlistview/MainActivity.java
+++ ParallaxHeaderListview/app/src/main/java/github/nisrulz/parallaxheaderlistview/MainActivity.java
@@ -1,13 +1,67 @@
 package github.nisrulz.parallaxheaderlistview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar androidToolbar;
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    CoordinatorLayout mRootLayout;
+    ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        initOtherLayoutInstances();
+        initListview();
+    }
+
+    private void initToolbar() {
+        androidToolbar = (Toolbar) findViewById(R.id.toolbar_android);
+        setSupportActionBar(androidToolbar);
+    }
+
+    private void initOtherLayoutInstances() {
+        mRootLayout = (CoordinatorLayout) findViewById(R.id.coordinatorRootLayout);
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayoutAndroidExample);
+        mCollapsingToolbarLayout.setTitle("Parallax Header Listview");
+    }
+
+    private void initListview() {
+        mListView = (ListView) findViewById(R.id.listView);
+        String[] listStringValue = new String[]{"Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
+                "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13",
+                "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20",
+                "Item 21", "Item 22", "Item 23", "Item 24", "Item 25"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listStringValue);
+
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String) mListView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  Text : " + itemValue, Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
