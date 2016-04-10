300b0f4a7ccb572f5bb148d258c6655abc1cbeab
Nishant Srivastava <nisrulz@gmail.com>
2016-04-10 11:13:47 +0530

    (*) implemented CollapsibleToolbar

--- CollapsibleToolbar/app/src/main/java/github/nisrulz/collapsibletoolbar/MainActivity.java
+++ CollapsibleToolbar/app/src/main/java/github/nisrulz/collapsibletoolbar/MainActivity.java
@@ -1,13 +1,30 @@
 package github.nisrulz.collapsibletoolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        collapsingToolbarLayout.setTitle("Animated Toolbar");
+
+        TextView textv_1 = (TextView) findViewById(R.id.textv_1);
+        textv_1.setText(getResources().getString(R.string.dummytext));
+
     }
 }
