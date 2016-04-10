06758d334d82a5ed8cecd6b71d26775dfcee27a6
Nishant Srivastava <nisrulz@gmail.com>
2016-04-10 12:21:45 +0530

    (*) implement GPSLocation

--- GPSLocation/app/src/main/java/github/nisrulz/gpslocation/MainActivity.java
+++ GPSLocation/app/src/main/java/github/nisrulz/gpslocation/MainActivity.java
@@ -1,7 +1,17 @@
 package github.nisrulz.gpslocation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupLocationListening();
+
+    }
+
+    void setupLocationListening() {
+
+        LocationManager locationManager;
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                TextView longitude = (TextView) findViewById(R.id.textView_lon);
+                TextView latitude = (TextView) findViewById(R.id.textView_lat);
+
+                longitude.setText("Longitude:" + location.getLongitude());
+                latitude.setText("Latitude:" + location.getLatitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        String mprovider;
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        mprovider = locationManager.getBestProvider(criteria, false);
+
+        if (mprovider != null && !mprovider.equals("")) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(mprovider);
+            locationManager.requestLocationUpdates(mprovider, 15000, 1, locationListener);
+
+            if (location != null)
+                locationListener.onLocationChanged(location);
+            else
+                Toast.makeText(getBaseContext(), "No Location Provider Found Check Your Code", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
