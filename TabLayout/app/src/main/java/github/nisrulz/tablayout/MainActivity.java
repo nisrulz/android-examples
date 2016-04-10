bebd67d09e2168bc682cd50cc9445b2081f6d152
Nishant Srivastava <nisrulz@gmail.com>
2016-04-10 11:23:00 +0530

    (*) implemented TabLayout

--- TabLayout/app/src/main/java/github/nisrulz/tablayout/MainActivity.java
+++ TabLayout/app/src/main/java/github/nisrulz/tablayout/MainActivity.java
@@ -1,13 +1,26 @@
 package github.nisrulz.tablayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
     }
 }
