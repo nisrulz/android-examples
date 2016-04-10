0d52dee8135396e587e0706578e60683022efbe7
Nishant Srivastava <nisrulz@gmail.com>
2016-04-10 11:40:09 +0530

    (*) implemented NavigationDrawer

--- NavigationDrawer/app/src/main/java/github/nisrulz/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/github/nisrulz/navigationdrawer/MainActivity.java
@@ -1,13 +1,64 @@
 package github.nisrulz.navigationdrawer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout androidDrawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initNavDrawerToggel();
+    }
+
+    private void initNavDrawerToggel() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        androidDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_design_support_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, androidDrawerLayout, R.string.app_name, R.string.app_name);
+        androidDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // This is required to make the drawer toggle work
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        /*
+         * if you have other menu items in your activity/toolbar
+         * handle them here and return true
+         */
+        return super.onOptionsItemSelected(item);
     }
 }
