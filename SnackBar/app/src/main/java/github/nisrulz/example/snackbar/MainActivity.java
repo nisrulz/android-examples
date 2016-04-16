1a9f4ec9f37adee262a16f6ded71ab8970470002
Nishant Srivastava <nisrulz@gmail.com>
2016-04-17 04:54:43 +0530

    (*) implemented Snackbar

--- SnackBar/app/src/main/java/github/nisrulz/example/snackbar/MainActivity.java
+++ SnackBar/app/src/main/java/github/nisrulz/example/snackbar/MainActivity.java
@@ -1,7 +1,10 @@
 package github.nisrulz.example.snackbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "This is a snackbar!", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
