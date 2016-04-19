753c4f7d649371b9d6c7bc5299cd0d651295c269
Nishant Srivastava <nisrulz@gmail.com>
2016-04-19 12:08:24 +0530

    (+) added Camera2

---
+++ Camera2/app/src/main/java/github/nisrulz/example/camera2/MainActivity.java
@@ -0,0 +1,40 @@
+package github.nisrulz.example.camera2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        goFullScreen(this);
+        hideActionBar(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2Fragment.newInstance())
+                    .commit();
+        }
+    }
+
+
+    void goFullScreen(Activity activity) {
+        // Call before calling setContentView();
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager
+                .LayoutParams
+                .FLAG_FULLSCREEN);
+    }
+
+    void hideActionBar(Activity activity) {
+        // Call before calling setContentView();
+        activity.getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+    }
+}
