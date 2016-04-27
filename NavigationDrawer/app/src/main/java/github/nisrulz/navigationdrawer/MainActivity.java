da0178fa4fec43d9e65835d188516b64ee1a44bb
Nishant Srivastava <nisrulz@gmail.com>
2016-04-27 11:28:45 +0530

    (*) updated NavigationDrawer

--- NavigationDrawer/app/src/main/java/github/nisrulz/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/github/nisrulz/navigationdrawer/MainActivity.java
@@ -2,16 +2,19 @@ package github.nisrulz.navigationdrawer;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout androidDrawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
     Toolbar toolbar;
 
     @Override
@@ -30,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
         androidDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_design_support_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, androidDrawerLayout, R.string.app_name, R.string.app_name);
         androidDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_menu_item1:
+                        Toast.makeText(MainActivity.this, "Item 1 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_menu_item2:
+                        Toast.makeText(MainActivity.this, "Item 2 Clicked", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case R.id.nav_menu_item3:
+                        Toast.makeText(MainActivity.this, "Item 3 Clicked", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
