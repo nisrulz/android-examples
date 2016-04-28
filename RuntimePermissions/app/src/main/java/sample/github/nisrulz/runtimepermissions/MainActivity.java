727a6106d078cef5c9604a8010fe65c5682c171c
Nishant Srivastava <nisrulz@gmail.com>
2016-04-28 15:55:11 +0530

    (*) implemented RuntimePermissions

--- RuntimePermissions/app/src/main/java/sample/github/nisrulz/runtimepermissions/MainActivity.java
+++ RuntimePermissions/app/src/main/java/sample/github/nisrulz/runtimepermissions/MainActivity.java
@@ -1,13 +1,19 @@
 package sample.github.nisrulz.runtimepermissions;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +28,41 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               checkForPermissionAndMakeACall();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    void checkForPermissionAndMakeACall() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "CALL permission available..making a call", Toast.LENGTH_SHORT).show();
+            makeACall();
+        } else {
+            requestCallPermission();
+        }
+    }
+
+    void requestCallPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if(permissions[0].equals(Manifest.permission.CALL_PHONE) && grantResults[0]==
+                    PackageManager.PERMISSION_GRANTED){
+                makeACall();
+            } else {
+                Toast.makeText(this, "Permission was Denied!", Toast.LENGTH_SHORT).show();
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void makeACall() {
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:90990990900"));
+        startActivity(i);
     }
 }
