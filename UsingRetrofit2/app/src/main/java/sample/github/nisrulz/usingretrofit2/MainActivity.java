e0b2ca415b42e276549085750f205a12e2ec788d
Nishant Srivastava <nisrulz@gmail.com>
2016-05-08 23:59:00 +0530

    (*) added a progress dialogue to UsingRetrofit2

--- UsingRetrofit2/app/src/main/java/sample/github/nisrulz/usingretrofit2/MainActivity.java
+++ UsingRetrofit2/app/src/main/java/sample/github/nisrulz/usingretrofit2/MainActivity.java
@@ -1,10 +1,12 @@
 package sample.github.nisrulz.usingretrofit2;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,9 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = MainActivity.class.getSimpleName();
 
+  private ProgressDialog progressDialog;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    progressDialog = new ProgressDialog(this);
+    progressDialog.setMessage("Loading..");
+    progressDialog.setIndeterminate(true);
+    progressDialog.setCanceledOnTouchOutside(false);
+    progressDialog.show();
 
     final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_peoples);
     recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -34,11 +44,14 @@ public class MainActivity extends AppCompatActivity {
       public void onResponse(Call<PeopleResponse> call, Response<PeopleResponse> response) {
         List<People> peoples = response.body().getPeoples();
         recyclerView.setAdapter(new ListAdapter(peoples, R.layout.list_item_people));
+        progressDialog.dismiss();
       }
 
       @Override public void onFailure(Call<PeopleResponse> call, Throwable t) {
         // Log error here since request failed
         Log.e(TAG, t.toString());
+        Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+        progressDialog.dismiss();
       }
     });
   }
