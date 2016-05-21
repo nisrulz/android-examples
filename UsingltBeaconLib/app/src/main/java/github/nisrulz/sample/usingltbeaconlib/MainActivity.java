a7442a9e346edc594f98fb9c0547df77b6b6f7da
Nishant Srivastava <nisrulz@gmail.com>
2016-05-22 01:29:12 +0530

    [*] updated UsingAltBeaconLib

--- UsingltBeaconLib/app/src/main/java/github/nisrulz/sample/usingltbeaconlib/MainActivity.java
+++ UsingltBeaconLib/app/src/main/java/github/nisrulz/sample/usingltbeaconlib/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
