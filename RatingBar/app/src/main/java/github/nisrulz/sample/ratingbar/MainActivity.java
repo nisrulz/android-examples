6be585e754aa1b089c890bcfa46826bc578b2e6d
Nishant Srivastava <nisrulz@gmail.com>
2016-05-22 17:43:42 +0530

    [*] implemented RatingBar

--- RatingBar/app/src/main/java/github/nisrulz/sample/ratingbar/MainActivity.java
+++ RatingBar/app/src/main/java/github/nisrulz/sample/ratingbar/MainActivity.java
@@ -1,12 +1,24 @@
 package github.nisrulz.sample.ratingbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  RatingBar ratingBar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+    ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+      @Override public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+        Toast.makeText(MainActivity.this, String.valueOf(v), Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
