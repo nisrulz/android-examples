a97dfcf650f882736681385c04c6b785bf72f1cf
Nishant Srivastava <nisrulz@gmail.com>
2016-05-22 17:49:20 +0530

    [*] implemented textview in RatingBar

--- RatingBar/app/src/main/java/github/nisrulz/sample/ratingbar/MainActivity.java
+++ RatingBar/app/src/main/java/github/nisrulz/sample/ratingbar/MainActivity.java
@@ -3,21 +3,24 @@ package github.nisrulz.sample.ratingbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RatingBar;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
   RatingBar ratingBar;
+
+  TextView txtstatus;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+    txtstatus = (TextView) findViewById(R.id.txtstatus);
 
     ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
       @Override public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-        Toast.makeText(MainActivity.this, String.valueOf(v), Toast.LENGTH_SHORT).show();
+        txtstatus.setText(String.valueOf(ratingBar.getRating()));
       }
     });
   }
