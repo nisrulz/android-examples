d8758d1b42cb2481863acc745fd026e4d0686419
Nishant Srivastava <nisrulz@gmail.com>
2016-06-02 14:29:20 +0530

    [*] updated UsingDBFlow example with DBFlow v3.0.1

--- UsingDBFlow/app/src/main/java/github/nisrulz/sample/usingdbflow/MyApplication.java
+++ UsingDBFlow/app/src/main/java/github/nisrulz/sample/usingdbflow/MyApplication.java
@@ -1,6 +1,7 @@
 package github.nisrulz.sample.usingdbflow;
 
 import android.app.Application;
+import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
 public class MyApplication extends Application {
@@ -8,6 +9,6 @@ public class MyApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
 
-    FlowManager.init(this);
+    FlowManager.init(new FlowConfig.Builder(this).build());
   }
 }
