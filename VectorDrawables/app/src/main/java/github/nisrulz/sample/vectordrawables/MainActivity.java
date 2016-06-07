25770c5b97dcf73d3873e6744a9c5ce06a6cc160
Nishant Srivastava <nisrulz@gmail.com>
2016-06-07 12:14:22 +0530

    [*] Implemented VectorDrawables

--- VectorDrawables/app/src/main/java/github/nisrulz/sample/vectordrawables/MainActivity.java
+++ VectorDrawables/app/src/main/java/github/nisrulz/sample/vectordrawables/MainActivity.java
@@ -2,11 +2,16 @@ package github.nisrulz.sample.vectordrawables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ImageView imgvw = (ImageView) findViewById(R.id.imgvw);
+    // Programmatically loading vector drawable
+    imgvw.setImageResource(R.drawable.ic_adb_black_24dp);
   }
 }
