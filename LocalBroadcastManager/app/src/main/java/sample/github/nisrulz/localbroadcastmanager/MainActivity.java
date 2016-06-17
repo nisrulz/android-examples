26e3942730f01735e99baf756e4a89f8ca5b6be0
Nishant Srivastava <nisrulz@gmail.com>
2016-06-17 17:41:41 +0530

    [*] implemented LocalBroadcastManager

--- LocalBroadcastManager/app/src/main/java/sample/github/nisrulz/localbroadcastmanager/MainActivity.java
+++ LocalBroadcastManager/app/src/main/java/sample/github/nisrulz/localbroadcastmanager/MainActivity.java
@@ -1,12 +1,56 @@
 package sample.github.nisrulz.localbroadcastmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button btn = (Button) findViewById(R.id.button);
+    btn.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        startService(new Intent(MainActivity.this, MyIntentService.class));
+      }
+    });
+  }
+
+  // Our handler for received Intents. This will be called whenever an Intent
+  // with an action named "custom-event-name" is broadcasted.
+  private BroadcastReceiver messageBroadcastReceiver = new BroadcastReceiver() {
+    @Override public void onReceive(Context context, Intent intent) {
+      // Get extra data included in the Intent
+      String message = intent.getStringExtra("message");
+      Toast.makeText(MainActivity.this, "Got message: " + message, Toast.LENGTH_SHORT).show();
+      Log.d("receiver", "Got message: " + message);
+    }
+  };
+
+  @Override protected void onPause() {
+    // Unregister since the activity is paused.
+    LocalBroadcastManager.getInstance(this).unregisterReceiver(messageBroadcastReceiver);
+    super.onPause();
+  }
+
+  @Override protected void onResume() {
+
+    // Register to receive messages.
+    // We are registering an observer (mMessageReceiver) to receive Intents
+    // with actions named "custom-event-name".
+    LocalBroadcastManager.getInstance(this)
+        .registerReceiver(messageBroadcastReceiver, new IntentFilter("custom-event-name"));
+
+    super.onResume();
   }
 }
---
+++ LocalBroadcastManager/app/src/main/java/sample/github/nisrulz/localbroadcastmanager/MyIntentService.java
@@ -0,0 +1,29 @@
+package sample.github.nisrulz.localbroadcastmanager;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class MyIntentService extends IntentService {
+  public MyIntentService() {
+    super("MyIntentService");
+  }
+
+  @Override protected void onHandleIntent(Intent intent) {
+    if (intent != null) {
+      sendMessage();
+    }
+  }
+
+  // Send an Intent with an action named "custom-event-name". The Intent
+  // sent should
+  // be received by the ReceiverActivity.
+  private void sendMessage() {
+    Log.d("sender", "Broadcasting message");
+    Intent intent = new Intent("custom-event-name");
+    // You can also include some extra data.
+    intent.putExtra("message", "This is my message!");
+    LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+  }
+}
