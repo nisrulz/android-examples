d357ffb4d01e5dbb820647ac9f0088b606331b2d
Nishant Srivastava <nisrulz@gmail.com>
2016-06-20 09:52:25 +0530

    [*] updated UsingDagger2

--- UsingDagger2/app/src/main/java/sample/github/nisrulz/usingdagger2/MainActivity.java
+++ UsingDagger2/app/src/main/java/sample/github/nisrulz/usingdagger2/MainActivity.java
@@ -1,12 +1,18 @@
 package sample.github.nisrulz.usingdagger2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .add(android.R.id.content, new MainFragment())
+          .commit();
+    }
   }
 }
