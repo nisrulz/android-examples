7e80f58d2ce4cc6830a6d6a91bbb1514c5851032
Nishant Srivastava <nisrulz@gmail.com>
2016-07-01 15:33:37 +0530

    [*] updated project Firebase to support api 24 and upgraded to latest firebase v9.2.0

--- Firebase/app/src/main/java/github/nisrulz/firebase/MainActivity.java
+++ Firebase/app/src/main/java/github/nisrulz/firebase/MainActivity.java
@@ -6,57 +6,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
-
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txt;
-    // Declare the Firebase instance
-    Firebase myFirebaseRef;
+  TextView txt;
 
-    // ----------- IMPORTANT ----------
-    // Feed the appropiate url of your firebase app here, or else the app wont run.
-    String firebaseURL = "https://<YOUR-FIREBASE-APP>.firebaseio.com/";
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-        txt = (TextView) findViewById(R.id.text);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        // Set the context for Firebase
-        Firebase.setAndroidContext(this);
+    txt = (TextView) findViewById(R.id.text);
 
-        // Init the firebase instance
-        myFirebaseRef = new Firebase(firebaseURL);
+    final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // On Click
-                // Write data to Firebase Database
-                myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
-            }
-        });
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        // On Click
+        // Write data to Firebase Database
+        rootRef.child("message").setValue("Do you have data? You'll love Firebase.");
+      }
+    });
 
-        // Read data from Firebase Database
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
-                txt.setText(snapshot.getValue().toString());
-            }
+    // Read data from Firebase Database
+    rootRef.child("message").addValueEventListener(new ValueEventListener() {
+      @Override public void onDataChange(DataSnapshot dataSnapshot) {
+        System.out.println(
+            dataSnapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+        txt.setText(dataSnapshot.getValue().toString());
+      }
 
-            @Override
-            public void onCancelled(FirebaseError error) {
-            }
-        });
-    }
+      @Override public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+  }
 }
