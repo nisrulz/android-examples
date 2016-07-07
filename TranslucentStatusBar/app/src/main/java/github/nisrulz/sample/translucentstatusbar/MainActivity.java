70a77145ba218815d4b94635b5ca4d8eaf2ffe3c
Nishant Srivastava <nisrulz@gmail.com>
2016-07-07 14:08:04 +0530

    [*] implemented TranslucentStatusToolbar

--- TranslucentStatusBar/app/src/main/java/github/nisrulz/sample/translucentstatusbar/MainActivity.java
+++ TranslucentStatusBar/app/src/main/java/github/nisrulz/sample/translucentstatusbar/MainActivity.java
@@ -1,12 +1,29 @@
 package github.nisrulz.sample.translucentstatusbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Setup this flag to enable making image extend into status bar
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      getWindow().getDecorView()
+          .setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
     setContentView(R.layout.activity_main);
+
+    // Find the toolbar view inside the activity layout
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    // Sets the Toolbar to act as the ActionBar for this Activity window.
+    // Make sure the toolbar exists in the activity and is not null
+    setSupportActionBar(toolbar);
   }
 }
