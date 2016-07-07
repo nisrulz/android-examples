cd7fdf83642312269b913f84da2ff11e19aa5860
Nishant Srivastava <nisrulz@gmail.com>
2016-07-07 15:12:47 +0530

    [*] updated code to set translucent statusbar in Api 21+ , updated imageview to be in conformance of toolbar height

--- TranslucentStatusBar/app/src/main/java/github/nisrulz/sample/translucentstatusbar/MainActivity.java
+++ TranslucentStatusBar/app/src/main/java/github/nisrulz/sample/translucentstatusbar/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
     // Sets the Toolbar to act as the ActionBar for this Activity window.
     // Make sure the toolbar exists in the activity and is not null
     setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+
+    return super.onCreateOptionsMenu(menu);
   }
 }
