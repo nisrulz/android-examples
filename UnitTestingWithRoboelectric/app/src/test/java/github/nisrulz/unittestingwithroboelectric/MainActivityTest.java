562c61c31bb61d5df5ca0c99fb0434913d419bf0
Nishant Srivastava <nisrulz@gmail.com>
2016-09-07 13:17:40 +0530

    [*] replaced the deprecated RobolectricGradleTestRunner with RobolectricTestRunner

--- UnitTestingWithRoboelectric/app/src/test/java/github/nisrulz/unittestingwithroboelectric/MainActivityTest.java
+++ UnitTestingWithRoboelectric/app/src/test/java/github/nisrulz/unittestingwithroboelectric/MainActivityTest.java
@@ -2,40 +2,35 @@ package github.nisrulz.unittestingwithroboelectric;
 
 import android.os.Build;
 import android.widget.TextView;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivityTest {
 
-    private MainActivity activity;
+  private MainActivity activity;
 
-    @Before
-    public void setup() {
-        activity = Robolectric.setupActivity(MainActivity.class);
-    }
+  @Before
+  public void setup() {
+    activity = Robolectric.setupActivity(MainActivity.class);
+  }
 
+  @Test
+  public void validateTextViewHasText() {
+    TextView tvHelloWorld = (TextView) activity.findViewById(R.id.tvHelloWorld);
+    assertNotNull("TextView could not be found", tvHelloWorld);
 
-    @Test
-    public void validateTextViewHasText() {
-        TextView tvHelloWorld = (TextView) activity.findViewById(R.id.tvHelloWorld);
-        assertNotNull("TextView could not be found", tvHelloWorld);
+    assertTrue("TextView contains incorrect text",
+        "Hello World!".equals(tvHelloWorld.getText().toString()));
 
-        assertTrue("TextView contains incorrect text",
-                "Hello World!".equals(tvHelloWorld.getText().toString()));
-
-
-        assertNotNull(activity);
-
-    }
-
+    assertNotNull(activity);
+  }
 }
