c6288aa2707ff0e0ba86e6d6e68cde63cd731a00
Nishant Srivastava <nisrulz@gmail.com>
2016-11-10 01:22:52 +0530

    [+] added UsingParceler

---
+++ UsingParceler/app/src/main/java/github/nisrulz/sample/usingparceler/DetailsActivity.java
@@ -0,0 +1,13 @@
+package github.nisrulz.sample.usingparceler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_details);
+  }
+}
