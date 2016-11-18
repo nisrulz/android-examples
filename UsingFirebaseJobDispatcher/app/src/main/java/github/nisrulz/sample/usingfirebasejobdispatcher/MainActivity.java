82ab021f23a40e38017a4d32ced48860c6140840
Nishant Srivastava <nisrulz@gmail.com>
2016-11-18 14:27:23 +0530

    [*] updated UsingFirebaseDispatcher example

--- UsingFirebaseJobDispatcher/app/src/main/java/github/nisrulz/sample/usingfirebasejobdispatcher/MainActivity.java
+++ UsingFirebaseJobDispatcher/app/src/main/java/github/nisrulz/sample/usingfirebasejobdispatcher/MainActivity.java
@@ -1,10 +1,13 @@
 package github.nisrulz.sample.usingfirebasejobdispatcher;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.firebase.jobdispatcher.Constraint;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
@@ -12,6 +15,7 @@ import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.RetryStrategy;
 import com.firebase.jobdispatcher.Trigger;
+import java.util.ArrayList;
 
 /**
  * The type Main activity.
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
    */
   FirebaseJobDispatcher dispatcher;
 
+  ListView listView;
+  ArrayList<String> actions;
+  ArrayAdapter<String> adapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -33,12 +41,48 @@ public class MainActivity extends AppCompatActivity {
     // Create a new dispatcher using the Google Play driver.
     dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
+    listView = (ListView) findViewById(R.id.listview);
+    actions = new ArrayList<>();
+    actions.add("Schedule a Simple Job");
+    actions.add("Cancel the Simple Job");
+    actions.add("Schedule a Complex Job");
+    actions.add("Cancel the Complex Job");
+    actions.add("Cancel all Jobs");
 
-        scheduleASimpleJob("my-unique-tag");
+    adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, actions);
+    listView.setAdapter(adapter);
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        switch (position) {
+          case 0:
+            scheduleASimpleJob("simple-job");
+            Toast.makeText(MainActivity.this, "Scheduled the Simple Job", Toast.LENGTH_SHORT)
+                .show();
+            break;
+          case 1:
+            cancelAJob("simple-job");
+            Toast.makeText(MainActivity.this, "Cancelling the Simple Job", Toast.LENGTH_SHORT)
+                .show();
+            break;
+          case 2:
+            Bundle extraBundle = new Bundle();
+            extraBundle.putString("some-key", "some-value");
+
+            scheduleAComplexJob("complex-job", extraBundle);
+            Toast.makeText(MainActivity.this, "Scheduled the Complex Job", Toast.LENGTH_SHORT)
+                .show();
+            break;
+          case 3:
+            cancelAJob("complex-job");
+            Toast.makeText(MainActivity.this, "Cancelling the Complex Job", Toast.LENGTH_SHORT)
+                .show();
+            break;
+          case 4:
+            cancelAllJobs();
+            Toast.makeText(MainActivity.this, "Cancelling all Jobs", Toast.LENGTH_SHORT).show();
+            break;
+        }
       }
     });
   }
--- UsingFirebaseJobDispatcher/app/src/main/java/github/nisrulz/sample/usingfirebasejobdispatcher/MyJobService.java
+++ UsingFirebaseJobDispatcher/app/src/main/java/github/nisrulz/sample/usingfirebasejobdispatcher/MyJobService.java
@@ -1,16 +1,19 @@
 package github.nisrulz.sample.usingfirebasejobdispatcher;
 
 import android.util.Log;
+import android.widget.Toast;
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 
 public class MyJobService extends JobService {
-  private static final String LOGTAG = "MyJobService";
+
+  private static final String LOGTAG = "My Job Service";
 
   @Override
   public boolean onStartJob(JobParameters job) {
     // Do some work here
-    Log.d(LOGTAG, "Job Started! Yay :D");
+    Toast.makeText(this, "Job Started! Yay :D", Toast.LENGTH_SHORT).show();
+    Log.i(LOGTAG, "Job Started! Yay :D");
 
     return false; // Answers the question: "Is there still work going on?"
   }
