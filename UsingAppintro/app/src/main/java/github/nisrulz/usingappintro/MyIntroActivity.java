a6cc883e105b96182398c2e7b7650b715b055307
Nishant Srivastava <nisrulz@gmail.com>
2016-11-22 12:30:39 +0530

    [*] updated UsingAppIntro as per the latest v4.1.0

--- UsingAppintro/app/src/main/java/github/nisrulz/usingappintro/MyIntroActivity.java
+++ UsingAppintro/app/src/main/java/github/nisrulz/usingappintro/MyIntroActivity.java
@@ -2,51 +2,95 @@ package github.nisrulz.usingappintro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.WindowManager;
-
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class MyIntroActivity extends AppIntro2 {
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    @Override
-    public void init(Bundle bundle) {
+    // Make it fullscreen
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Make it fullscreen
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    // Define Titles
+    String[] titles = {
+        getResources().getString(R.string.title_1), getResources().getString(R.string.title_2),
+        getResources().getString(R.string.title_3), getResources().getString(R.string.title_4)
+    };
 
-        String[] titles = {getResources().getString(R.string.title_1), getResources().getString(R
-                .string.title_2), getResources().getString(R.string.title_3), getResources()
-                .getString(R.string.title_4)};
-        String[] desc = {getResources().getString(R.string.desc_1), getResources().getString(R
-                .string.desc_2), getResources().getString(R.string.desc_3), getResources()
-                .getString(R.string.desc_4)};
+    // Define Descriptions
+    String[] desc = {
+        getResources().getString(R.string.desc_1), getResources().getString(R.string.desc_2),
+        getResources().getString(R.string.desc_3), getResources().getString(R.string.desc_4)
+    };
 
-        addSlide(AppIntroFragment.newInstance(titles[0], desc[0], R.drawable.p1,
-                R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance(titles[1], desc[1], R.drawable.p2,
-                R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance(titles[2], desc[2], R.drawable.p3,
-                R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance(titles[3], desc[3], R.drawable.p4,
-                R.color.colorPrimary));
+    // Define Images
+    int[] image = {
+        R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4
+    };
 
-    }
+    // Define Colors
+    int[] color = {
+        ContextCompat.getColor(this, R.color.colorPrimary),
+        ContextCompat.getColor(this, R.color.colorAccent),
+        ContextCompat.getColor(this, R.color.colorPrimary),
+        ContextCompat.getColor(this, R.color.colorPrimaryDark)
+    };
 
-    @Override
-    public void onNextPressed() {
+    // Start Adding Intro Slides
+    addSlide(AppIntroFragment.newInstance(titles[0], desc[0], image[0], color[0]));
+    addSlide(AppIntroFragment.newInstance(titles[1], desc[1], image[1], color[1]));
+    addSlide(AppIntroFragment.newInstance(titles[2], desc[2], image[2], color[2]));
+    addSlide(AppIntroFragment.newInstance(titles[3], desc[3], image[3], color[3]));
 
-    }
+    // Hide Skip/Done button.
+    showSkipButton(true);
+    setProgressButtonEnabled(true);
 
-    @Override
-    public void onDonePressed() {
-        startActivity(new Intent(MyIntroActivity.this, MainActivity.class));
+    // Turn vibration on and set intensity.
+    // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+    setVibrate(true);
+    setVibrateIntensity(30);
 
-    }
+    // Available Slide Change Animations
+    //setFadeAnimation();
+    //setZoomAnimation();
+    //setFlowAnimation();
+    //setSlideOverAnimation();
+    setDepthAnimation();
+  }
 
-    @Override
-    public void onSlideChanged() {
+  @Override
+  public void onSkipPressed(Fragment currentFragment) {
+    super.onSkipPressed(currentFragment);
+    // Do something when users tap on Skip button.
 
-    }
+    startMainActivity();
+  }
+
+  @Override
+  public void onDonePressed(Fragment currentFragment) {
+    super.onDonePressed(currentFragment);
+    // Do something when users tap on Done button.
+
+    startMainActivity();
+  }
+
+  @Override
+  public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+    super.onSlideChanged(oldFragment, newFragment);
+    // Do something when the slide changes.
+  }
+
+  private void startMainActivity() {
+    // Start the MainActivity and close this activity
+    startActivity(new Intent(MyIntroActivity.this, MainActivity.class));
+    finish();
+  }
 }
