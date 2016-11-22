71455cc15e1cc31a2f803ff6b053fc580994c8ec
Nishant Srivastava <nisrulz@gmail.com>
2016-11-22 14:14:57 +0530

    [*] cleanup

---
+++ UsingFirebaseJobDispatcher/app/src/test/java/github/nisrulz/sample/usingfirebasejobdispatcher/MyJobServiceTest.java
@@ -0,0 +1,15 @@
+package github.nisrulz.sample.usingfirebasejobdispatcher;
+
+import org.junit.Test;
+
+public class MyJobServiceTest {
+  @Test
+  public void onStartJob() throws Exception {
+
+  }
+
+  @Test
+  public void onStopJob() throws Exception {
+
+  }
+}
