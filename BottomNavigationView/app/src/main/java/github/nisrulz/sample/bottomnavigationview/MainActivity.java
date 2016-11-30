9d20c59d9e35e72509f7caaea246c71029ef4752
Nishant Srivastava <nisrulz@gmail.com>
2016-11-30 13:09:20 +0530

    [*] implemented BottomNavigationView

--- BottomNavigationView/app/src/main/java/github/nisrulz/sample/bottomnavigationview/MainActivity.java
+++ BottomNavigationView/app/src/main/java/github/nisrulz/sample/bottomnavigationview/MainActivity.java
@@ -1,7 +1,11 @@
 package github.nisrulz.sample.bottomnavigationview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    final TextView textView = (TextView) findViewById(R.id.textView);
+
+    // Declare and reference the bottom navigation view
+    BottomNavigationView bottomNavigationView =
+        (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+    //Attach the listener
+    bottomNavigationView.setOnNavigationItemSelectedListener(
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+          @Override
+          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+              case R.id.update_item:
+                textView.setText("Update Item Selected");
+                break;
+              case R.id.location_item:
+                textView.setText("Location Item Selected");
+                break;
+              case R.id.favorite_item:
+                textView.setText("Favorite Item Selected");
+                break;
+            }
+            return true;
+          }
+        });
   }
 }
