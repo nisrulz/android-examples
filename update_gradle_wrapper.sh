0659460705a0dcedc918bdadc373e069dfa54597
Nishant Srivastava <nisrulz@gmail.com>
2017-03-02 00:59:58 -0800

    updated all example apps to gradle v3.4

--- update_gradle_wrapper.sh
+++ update_gradle_wrapper.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Written by Nishant Srivastava
 
-#  Call as 
-# ./update_gradle_wrapper.sh 3.1
+#  Call as
+# ./update_gradle_wrapper.sh 3.4
 
 # Iterate over each child directory inside the current directory
 echo "# Updating to Gradle Wrapper Version: $1"
 echo ""
-for dir in ./*; 
-do 
+for dir in ./*;
+do
 	echo ""
 	echo ">>>> Updating directory : " $dir
-	(cd "$dir" && ./gradlew wrapper --gradle-version $1 --distribution-type all && ./gradlew); 
+	(cd "$dir" && ./gradlew wrapper --gradle-version $1 --distribution-type all && ./gradlew);
 	echo ""
 	echo ">>>> Done."
 done
