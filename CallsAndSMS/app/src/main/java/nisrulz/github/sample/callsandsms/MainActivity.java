1c7ce13a719e3f867137d99a8410eed8154ef2cd
Nishant Srivastava <nisrulz@gmail.com>
2017-03-07 21:51:38 -0800

    added implementation for listening for call states in CallsAndSMS example

--- CallsAndSMS/app/src/main/java/nisrulz/github/sample/callsandsms/MainActivity.java
+++ CallsAndSMS/app/src/main/java/nisrulz/github/sample/callsandsms/MainActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
   private EditText etPhoneNo;
   private Button btnDial;
   private Button btnCall;
+  private TelephonyManager telephonyManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     initView();
+
+    telephonyManager =
+        (TelephonyManager) getSystemService(getApplicationContext().TELEPHONY_SERVICE);
   }
 
   private void initView() {
@@ -59,8 +65,10 @@ public class MainActivity extends AppCompatActivity {
           if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
               == PackageManager.PERMISSION_GRANTED) {
             startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
-          }else{
-            Toast.makeText(MainActivity.this, "Permission not granted yet!", Toast.LENGTH_SHORT).show();
+          }
+          else {
+            Toast.makeText(MainActivity.this, "Permission not granted yet!", Toast.LENGTH_SHORT)
+                .show();
           }
         }
         else {
@@ -69,4 +77,37 @@ public class MainActivity extends AppCompatActivity {
       }
     });
   }
+
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
+  }
+
+  PhoneStateListener phoneStateListener = new PhoneStateListener() {
+    @Override
+    public void onCallStateChanged(int state, String incomingNumber) {
+      super.onCallStateChanged(state, incomingNumber);
+
+      switch (state) {
+        case TelephonyManager.CALL_STATE_IDLE:
+          Toast.makeText(MainActivity.this, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
+          break;
+        case TelephonyManager.CALL_STATE_RINGING:
+          Toast.makeText(MainActivity.this, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
+          break;
+        case TelephonyManager.CALL_STATE_OFFHOOK:
+          Toast.makeText(MainActivity.this, "CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
+          break;
+      }
+    }
+  };
 }
