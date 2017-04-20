ec34142115ce8f94f7cfee4619af6f7e42b145a1
Nishant Srivastava <nisrulz@gmail.com>
2017-04-19 23:29:10 -0700

    implemented PopupMenu

--- PopupMenu/app/src/main/java/nisrulz/github/example/popupmenu/MainActivity.java
+++ PopupMenu/app/src/main/java/nisrulz/github/example/popupmenu/MainActivity.java
@@ -1,13 +1,45 @@
 package nisrulz.github.example.popupmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+  private Button button1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    button1 = (Button) findViewById(R.id.button1);
+    button1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //Creating the instance of PopupMenu
+        PopupMenu popup = new PopupMenu(MainActivity.this, button1);
+        //Inflating the Popup using xml file
+        popup.getMenuInflater()
+            .inflate(R.menu.popup_menu, popup.getMenu());
+
+        //registering popup with OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+          public boolean onMenuItemClick(MenuItem item) {
+            Toast.makeText(
+                MainActivity.this,
+                "You Clicked : " + item.getTitle(),
+                Toast.LENGTH_SHORT
+            ).show();
+            return true;
+          }
+        });
+
+        popup.show(); //showing popup menu
+      }
+    }); //closing the setOnClickListener method
   }
 }
