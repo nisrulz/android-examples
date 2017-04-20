524b9599d4a7004c3058d3d67617ef3ce761c5e5
Nishant Srivastava <nisrulz@gmail.com>
2017-04-19 23:37:52 -0700

    added RadioButton

---
+++ RadioButton/app/src/androidTest/java/nisrulz/github/example/radiobutton/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package nisrulz.github.example.radiobutton;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("nisrulz.github.example.radiobutton", appContext.getPackageName());
+  }
+}
