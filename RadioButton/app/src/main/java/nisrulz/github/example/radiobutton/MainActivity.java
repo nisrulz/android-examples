fb3f8388bfc85da74e8a5c90a2eb05d28d83167f
Nishant Srivastava <nisrulz@gmail.com>
2017-04-19 23:38:52 -0700

    implemented RadioButton

--- RadioButton/app/src/main/java/nisrulz/github/example/radiobutton/MainActivity.java
+++ RadioButton/app/src/main/java/nisrulz/github/example/radiobutton/MainActivity.java
@@ -4,10 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+  private RadioGroup radioGroup;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    /* Initialize Radio Group and attach click handler */
+    radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+    radioGroup.clearCheck();
+
+    radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(RadioGroup radioGroup, @IdRes int checkedId) {
+        RadioButton rb = (RadioButton) radioGroup.findViewById(checkedId);
+        if(null!=rb && checkedId > -1){
+          Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+        }
+      }
+    });
+  }
+
+  public void onClear(View v) {
+        /* Clears all selected radio buttons to default */
+    radioGroup.clearCheck();
+  }
+
+  public void onSubmit(View v) {
+    RadioButton rb = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+    Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
   }
 }
