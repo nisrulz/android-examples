b514380d729b94f935734c778cb6ab1e243ce571
Nishant Srivastava <nisrulz@gmail.com>
2017-05-12 23:37:50 -0700

    delete the layout file for UsingLitho Example

--- UsingLitho/app/src/main/java/nisrulz/github/example/usinglitho/MainActivity.java
+++ UsingLitho/app/src/main/java/nisrulz/github/example/usinglitho/MainActivity.java
@@ -8,6 +8,5 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
   }
 }
