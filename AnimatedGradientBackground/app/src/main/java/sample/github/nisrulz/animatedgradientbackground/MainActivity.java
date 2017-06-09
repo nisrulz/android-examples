9443f657278fce000a4621e022eaa5f0bf5a1165
Nishant Srivastava <nisrulz@gmail.com>
2017-06-08 23:00:53 -0700

    implemented AnimatedGradientBackground

--- AnimatedGradientBackground/app/src/main/java/sample/github/nisrulz/animatedgradientbackground/MainActivity.java
+++ AnimatedGradientBackground/app/src/main/java/sample/github/nisrulz/animatedgradientbackground/MainActivity.java
@@ -1,13 +1,49 @@
 package sample.github.nisrulz.animatedgradientbackground;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+  private ConstraintLayout constraintLayout;
+  private AnimationDrawable animationDrawable;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getSupportActionBar().hide();
+
+    // init constraintLayout
+    constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
+    // initializing animation drawable by getting background from constraint layout
+    animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+    // setting enter fade animation duration to 5 seconds
+    animationDrawable.setEnterFadeDuration(5000);
+
+    // setting exit fade animation duration to 2 seconds
+    animationDrawable.setExitFadeDuration(2000);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (animationDrawable != null && !animationDrawable.isRunning()) {
+      // start the animation
+      animationDrawable.start();
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (animationDrawable != null && animationDrawable.isRunning()) {
+      // stop the animation
+      animationDrawable.stop();
+    }
   }
 }
