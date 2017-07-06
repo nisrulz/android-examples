d48240325ccc2de56e836911e3f3074fccef840c
Nishant Srivastava <nisrulz@gmail.com>
2017-07-06 14:21:44 -0700

    for UsingRoomORM example: annotate Person data class with Room annotations

--- UsingRoomORM/app/src/main/java/nisrulz/github/sample/usingroomorm/Person.java
+++ UsingRoomORM/app/src/main/java/nisrulz/github/sample/usingroomorm/Person.java
@@ -1,7 +1,11 @@
 package nisrulz.github.sample.usingroomorm;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
 public class Person {
-  String name;
+  @PrimaryKey String name;
   int age;
   String address;
 }
