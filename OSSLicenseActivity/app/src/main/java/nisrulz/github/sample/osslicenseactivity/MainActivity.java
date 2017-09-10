ace1bbe8909ff134af8e812d7c0186dad02c755f
Nishant Srivastava <nisrulz@gmail.com>
2017-09-10 13:53:38 -0700

    implemented OSSLicenseActivity example

--- OSSLicenseActivity/app/src/main/java/nisrulz/github/sample/osslicenseactivity/MainActivity.java
+++ OSSLicenseActivity/app/src/main/java/nisrulz/github/sample/osslicenseactivity/MainActivity.java
@@ -1,7 +1,11 @@
 package nisrulz.github.sample.osslicenseactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button btn_oss = findViewById(R.id.btn_oss);
+    btn_oss.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // OssLicensesMenuActivity is provided by google play services, just start the activity
+        Intent intent = new Intent(MainActivity.this, OssLicensesMenuActivity.class);
+        String title = getString(R.string.custom_license_title);
+        // Add a custom title to the activity
+        intent.putExtra("title", title);
+        startActivity(intent);
+      }
+    });
   }
 }
