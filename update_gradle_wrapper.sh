6b9aab0d505fbda5a524c3541cf7b5f33d1b38fa
Nishant Srivastava <nisrulz@gmail.com>
2017-10-31 22:53:18 -0700

    improved the update_gradle_wrapper script

--- update_gradle_wrapper.sh
+++ update_gradle_wrapper.sh
@@ -2,16 +2,24 @@
 # Written by Nishant Srivastava
 
 #  Call as
-# ./update_gradle_wrapper.sh 3.4
+# ./update_gradle_wrapper.sh
+
+echo "Update gradle wrapper to which version?"
+read version
 
 # Iterate over each child directory inside the current directory
-echo "# Updating to Gradle Wrapper Version: $1"
+echo "# Updating to Gradle Wrapper Version: $version"
 echo ""
 for dir in ./*;
 do
 	echo ""
 	echo ">>>> Updating directory : " $dir
-	(cd "$dir" && ./gradlew clean && ./gradlew wrapper --gradle-version $1 --distribution-type all && ./gradlew);
+	(cd "$dir" && ./gradlew clean && ./gradlew wrapper --gradle-version $version --distribution-type all && ./gradlew);
 	echo ""
 	echo ">>>> Done."
 done
+
+# Delete all generated build folders, because they will eat up a lot of space on the disc
+echo "# Deleting all build folders"
+echo ""
+./delete_build_folder.sh 
