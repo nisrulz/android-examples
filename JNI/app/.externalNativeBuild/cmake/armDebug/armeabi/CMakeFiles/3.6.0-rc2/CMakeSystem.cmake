d47f6f1225d5da275012b5348990aae811d5c662
Nishant Srivastava <nisrulz@gmail.com>
2017-11-19 17:55:01 -0800

    updated: gradle wrapper to v4.3.1 and android sdk path in all projects

--- JNI/app/.externalNativeBuild/cmake/armDebug/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ JNI/app/.externalNativeBuild/cmake/armDebug/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "16.6.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/nishant/android_sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("/Users/nishant/sdks/android_sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-14")
 set(CMAKE_SYSTEM_NAME "Android")
