6a27d2654176d97c93434209c48106fdaa07e0b4
Nishant Srivastava <nisrulz@gmail.com>
2018-01-22 00:16:55 +0530

    updated: Notification example

--- Notification/app/src/main/java/sample/github/nisrulz/notification/MainActivity.java
+++ Notification/app/src/main/java/sample/github/nisrulz/notification/MainActivity.java
@@ -1,11 +1,6 @@
 package sample.github.nisrulz.notification;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,77 +8,51 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-  int numMessages = 0;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    int numMessages = 0;
 
-    Button btn = (Button) findViewById(R.id.button);
-    btn.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        sendNotification("This is a Notification!");
-        sendBigNotification("This is a Big Notification!");
-      }
-    });
-  }
+    public static final String PERSONAL_CHANNEL_ID = "PERSONAL";
 
-  private void sendNotification(String messageBody) {
-    Intent intent = new Intent(this, MainActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    PendingIntent pendingIntent =
-        PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-    Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-    NotificationCompat.Builder notificationBuilder =
-        new NotificationCompat.Builder(this).setContentTitle("Notification")
-            .setContentText(messageBody)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent);
-    NotificationManager notificationManager =
-        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(0, notificationBuilder.build());
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-  private void sendBigNotification(String messageBody) {
-    Intent intent = new Intent(this, MainActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    PendingIntent pendingIntent =
-        PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-    Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        final NotificationUtils notificationUtils = new NotificationUtils(this);
+        notificationUtils.createChannel(PERSONAL_CHANNEL_ID, "PERSONAL");
 
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Simple Notification
+                notificationUtils.sendNotificationInDefaultChannel("Simple Notification", "Hello Simple World!", 101);
 
-     /* Add Big View Specific Configuration */
-    NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+                // Big Notification
+                notificationUtils.sendBigNotificationInDefaultChannel("Big Notification", "Hello BIG World!", 102);
 
-    String[] events = new String[6];
-    events[0] = new String("This is first line....");
-    events[1] = new String("This is second line...");
-    events[2] = new String("This is third line...");
-    events[3] = new String("This is 4th line...");
-    events[4] = new String("This is 5th line...");
-    events[5] = new String("This is 6th line...");
-
-    // Sets a title for the Inbox style big view
-    inboxStyle.setBigContentTitle("Big Title Details:");
-
-    // Moves events into the big view
-    for (int i = 0; i < events.length; i++) {
-      inboxStyle.addLine(events[i]);
+                // Custom Notifications
+                createCustomNotification(notificationUtils);
+            }
+        });
     }
 
-    NotificationCompat.Builder notificationBuilder =
-        new NotificationCompat.Builder(this).setContentTitle("Notification")
-            .setContentText(messageBody)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setTicker("New Message Alert!")
-            .setNumber(++numMessages)
-            .setSound(defaultSoundUri)
-            .setStyle(inboxStyle)
-            .setContentIntent(pendingIntent);
+    private void createCustomNotification(NotificationUtils notificationUtils) {
+        Intent resultIntent = new Intent(MainActivity.this, MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-    NotificationManager notificationManager =
-        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(1, notificationBuilder.build());
-  }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),
+                PERSONAL_CHANNEL_ID)
+                // set title
+                .setContentTitle("Custom Notification")
+                // set body
+                .setContentText("Hello Custom World!")
+                // set ticker
+                .setTicker("Custom Notification")
+                // set small icon
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                // set auto cancel behaviour
+                .setAutoCancel(true);
+        notificationUtils.sendNotificationInChannel(103, resultIntent, builder);
+    }
 }
---
+++ Notification/app/src/main/java/sample/github/nisrulz/notification/NotificationUtils.java
@@ -0,0 +1,131 @@
+package sample.github.nisrulz.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationUtils extends ContextWrapper {
+
+    private NotificationManager notificationManager;
+
+    String DEFAULT_CHANNEL_ID = "DEFAULT";
+
+    String DEFAULT_CHANNEL_NAME = "DEFAULT CHANNEL";
+
+    String pkgName = "notification.channel";
+
+
+    public NotificationUtils(Context context) {
+        super(context);
+        pkgName = context.getPackageName() != null ? context.getPackageName() : pkgName;
+        DEFAULT_CHANNEL_ID = pkgName + "." + DEFAULT_CHANNEL_ID.toUpperCase();
+        createChannel(DEFAULT_CHANNEL_ID, DEFAULT_CHANNEL_NAME);
+    }
+
+    public void createChannel(String CHANNEL_ID, String CHANNEL_NAME) {
+        CHANNEL_ID = pkgName + "." + CHANNEL_ID;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // create android channel
+            NotificationChannel newChannel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            // Sets whether notifications posted to this channel should display notification lights
+            newChannel.enableLights(true);
+            // Sets whether notification posted to this channel should vibrate.
+            newChannel.enableVibration(true);
+            // Sets the notification light color for notifications posted to this channel
+            newChannel.setLightColor(Color.GREEN);
+            // Sets whether notifications posted to this channel appear on the lockscreen or not
+            newChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            getNotificationManager().createNotificationChannel(newChannel);
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return notificationManager;
+    }
+
+    public void sendNotificationInChannel(int notificationId, Intent resultIntent,
+            NotificationCompat.Builder builder) {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent
+                .FLAG_UPDATE_CURRENT);
+
+        // for notification click action, also required on Gingerbread and below
+        builder.setContentIntent(pi);
+
+        getNotificationManager().notify(notificationId, builder.build());
+    }
+
+    public void sendNotificationInDefaultChannel(String title, String body, int notificationId) {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        sendNotificationInChannel(notificationId, resultIntent,
+                getDefaultNotificationBuilder(title, body, DEFAULT_CHANNEL_ID));
+    }
+
+
+    public void sendBigNotificationInDefaultChannel(String title, String body, int notificationId) {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        NotificationCompat.Builder builder = getDefaultNotificationBuilder(title, body, DEFAULT_CHANNEL_ID);
+        builder = convertToBigNotificationBuilder(builder);
+        sendNotificationInChannel(notificationId, resultIntent, builder);
+    }
+
+
+    private NotificationCompat.Builder getDefaultNotificationBuilder(String title, String body, String
+            channelId) {
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),
+                channelId)
+                // set title
+                .setContentTitle(title)
+                // set body
+                .setContentText(body)
+                // set ticker
+                .setTicker("Notification")
+                // set notification sound
+                .setSound(defaultSoundUri)
+                // set small icon
+                .setSmallIcon(android.R.drawable.stat_notify_more)
+                // set auto cancel behaviour
+                .setAutoCancel(true);
+        return builder;
+    }
+
+
+    private NotificationCompat.Builder convertToBigNotificationBuilder(NotificationCompat.Builder builder) {
+        /* Add Big View Specific Configuration */
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        String[] events = new String[6];
+        events[0] = new String("This is 1st line...");
+        events[1] = new String("This is 2nd line...");
+        events[2] = new String("This is 3rd line...");
+        events[3] = new String("This is 4th line...");
+        events[4] = new String("This is 5th line...");
+        events[5] = new String("This is 6th line...");
+
+        // Sets a title for the Inbox style big view
+        inboxStyle.setBigContentTitle("Big Title Details:");
+
+        // Moves events into the big view
+        for (int i = 0; i < events.length; i++) {
+            inboxStyle.addLine(events[i]);
+        }
+
+        builder.setStyle(inboxStyle);
+        return builder;
+    }
+}
+
