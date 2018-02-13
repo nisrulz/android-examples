6621a59279feb21a8a0cda5ed53aef8211dbc649
Nishant Srivastava <nisrulz@gmail.com>
2018-02-13 15:44:12 +0530

    added: CheckIfPhoneOrTablet example

---
+++ CheckIfPhoneOrTablet/app/src/main/java/github/nisrulz/sample/checkifphoneortablet/MainActivity.java
@@ -0,0 +1,18 @@
+package github.nisrulz.sample.checkifphoneortablet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+}
