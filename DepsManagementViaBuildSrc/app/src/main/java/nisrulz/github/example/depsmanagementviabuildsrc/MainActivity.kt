2d139b7ec27f44296ecc8405f1c0c7e41e55b053
Nishant Srivastava <nisrulz@gmail.com>
2018-04-08 14:43:24 +0530

    added: DepsManagementViaBuildSrc example

---
+++ DepsManagementViaBuildSrc/app/src/main/java/nisrulz/github/example/depsmanagementviabuildsrc/MainActivity.kt
@@ -0,0 +1,12 @@
+package nisrulz.github.example.depsmanagementviabuildsrc
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
