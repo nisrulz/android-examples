d8b5548b3bb2435c7bb80189849522ba9216d438
Nishant Srivastava <nisrulz@gmail.com>
2019-11-23 16:06:17 +0100

    wireup the ui with MainActivity in code

--- CommonConfigForModules/app/src/main/java/github/nisrulz/modulecommonconfig/MainActivity.kt
+++ CommonConfigForModules/app/src/main/java/github/nisrulz/modulecommonconfig/MainActivity.kt
@@ -2,11 +2,15 @@ package github.nisrulz.modulecommonconfig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayStr = ""
+        txt.text = displayStr
     }
 }
