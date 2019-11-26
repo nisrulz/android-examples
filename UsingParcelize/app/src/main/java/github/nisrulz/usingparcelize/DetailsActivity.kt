08f98410a106d7e539bc2d172eeaf137bcfa2ae0
Nishant Srivastava <nisrulz@gmail.com>
2019-11-26 01:44:10 +0100

    retrieve parcelable value from intent in details activity

--- UsingParcelize/app/src/main/java/github/nisrulz/usingparcelize/DetailsActivity.kt
+++ UsingParcelize/app/src/main/java/github/nisrulz/usingparcelize/DetailsActivity.kt
@@ -11,5 +11,9 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        val person = intent.extras?.getParcelable<Parcelable>(getString(R.string.person_detail_key)) as Person?
+        person?.let {
+            Toast.makeText(this, person.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
