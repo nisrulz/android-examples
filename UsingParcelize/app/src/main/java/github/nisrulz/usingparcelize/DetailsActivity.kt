47baf921c444452e9506abfa1c79408dff0ccf96
Nishant Srivastava <nisrulz@gmail.com>
2019-11-26 01:43:07 +0100

    setup the app to handle view and user interaction

---
+++ UsingParcelize/app/src/main/java/github/nisrulz/usingparcelize/DetailsActivity.kt
@@ -0,0 +1,15 @@
+package github.nisrulz.usingparcelize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcelable
+import android.widget.Toast
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+    }
+}
---
+++ UsingParcelize/app/src/main/java/github/nisrulz/usingparcelize/Person.kt
@@ -0,0 +1,15 @@
+package github.nisrulz.usingparcelize
+
+import android.os.Parcelable
+
+data class Person(
+    val name: String = "",
+    val address: String = "",
+    val age: Int = 0,
+    val occupation: String = ""
+) : Parcelable {
+
+    override fun toString(): String {
+        return "Name: $name\nAge: $age\nAddress: $address\nOccupation: $occupation".toString()
+    }
+}
