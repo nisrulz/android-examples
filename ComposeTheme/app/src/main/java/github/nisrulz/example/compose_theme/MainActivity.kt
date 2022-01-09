fd23b2ea094ec131678477231f5b2161a52b03b4
Nishant Srivastava <nisrulz@gmail.com>
2022-01-09 20:09:33 +0100

    add base example

---
+++ ComposeTheme/app/src/main/java/github/nisrulz/example/compose_theme/MainActivity.kt
@@ -0,0 +1,38 @@
+package github.nisrulz.example.compose_theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import github.nisrulz.example.compose_theme.ui.theme.ComposeThemeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeThemeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeThemeTheme {
+        Greeting("Android")
+    }
+}
