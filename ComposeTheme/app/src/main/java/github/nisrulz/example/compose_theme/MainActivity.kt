c21e3f38fbbfc6a950ef618645f68a9f752d2cd3
Nishant Srivastava <nisrulz@gmail.com>
2022-01-09 20:14:37 +0100

    setup custom theme properties

--- ComposeTheme/app/src/main/java/github/nisrulz/example/compose_theme/MainActivity.kt
+++ ComposeTheme/app/src/main/java/github/nisrulz/example/compose_theme/MainActivity.kt
@@ -3,12 +3,15 @@ package github.nisrulz.example.compose_theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import github.nisrulz.example.compose_theme.ui.theme.ComposeThemeTheme
+import github.nisrulz.example.compose_theme.ui.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +29,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        modifier = Modifier.padding(MaterialTheme.spacing.large),
+        text = "Hello $name!"
+    )
 }
 
 @Preview(showBackground = true)
--- ComposeTheme/app/src/main/java/github/nisrulz/example/compose_theme/ui/theme/Theme.kt
+++ ComposeTheme/app/src/main/java/github/nisrulz/example/compose_theme/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -37,11 +38,14 @@ fun ComposeThemeTheme(
     } else {
         LightColorPalette
     }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing(),
+    ) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
