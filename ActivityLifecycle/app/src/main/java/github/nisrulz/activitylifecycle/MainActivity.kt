71f2fe7160b363c78d9bee909da70faaac15d838
Nishant Srivastava <nisrulz@gmail.com>
2022-01-11 00:05:23 +0100

    use view binding in ActivtyLifecycle example

--- ActivityLifecycle/app/src/main/java/github/nisrulz/activitylifecycle/MainActivity.kt
+++ ActivityLifecycle/app/src/main/java/github/nisrulz/activitylifecycle/MainActivity.kt
@@ -2,28 +2,23 @@ package github.nisrulz.activitylifecycle
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import github.nisrulz.activitylifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        LogData("onCreate Called")
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+            setSupportActionBar(toolbar)
         }
+
+        LogData("onCreate Called")
     }
 
     override fun onStart() {
@@ -51,23 +46,7 @@ class MainActivity : AppCompatActivity() {
         LogData("onDestroy Called")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-    }
-
-    fun LogData(data: String?) {
+    private fun LogData(data: String?) {
         Toast.makeText(this@MainActivity, data, Toast.LENGTH_SHORT).show()
         Log.d("TAG", data!!)
     }
