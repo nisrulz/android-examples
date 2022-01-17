eb2921537be6a7380c3f703bfd0463f944758c32
Nishant Srivastava <nisrulz@gmail.com>
2022-01-18 00:24:08 +0100

    updated BottomNavigationView example

---
+++ BottomNavigationView/app/src/main/java/github/nisrulz/example/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,28 @@
+package github.nisrulz.example.bottomnavigationview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import github.nisrulz.example.bottomnavigationview.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+
+            bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.update_item -> textView.text = "Update Item Selected"
+                    R.id.location_item -> textView.text = "Location Item Selected"
+                    R.id.favorite_item -> textView.text = "Favorite Item Selected"
+                }
+                true
+            }
+
+        }
+    }
+}
