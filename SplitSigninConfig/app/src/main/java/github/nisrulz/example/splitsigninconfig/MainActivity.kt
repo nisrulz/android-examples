7cd003cfe9612b158fc4ed065787af90d2cb4ea4
Nishant Srivastava <nisrulz@gmail.com>
2022-03-02 22:04:38 +0100

    updated SplitSigningConfig

---
+++ SplitSigninConfig/app/src/main/java/github/nisrulz/example/splitsigninconfig/MainActivity.kt
@@ -0,0 +1,17 @@
+package github.nisrulz.example.splitsigninconfig
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import github.nisrulz.example.splitsigninconfig.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+        }
+    }
+}
