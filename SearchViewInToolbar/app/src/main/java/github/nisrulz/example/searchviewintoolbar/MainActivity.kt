558ae51e3088a653f1573e1801b074357af5656d
Nishant Srivastava <nisrulz@gmail.com>
2022-03-03 18:17:12 +0100

    updated SearchViewInToolbar example

---
+++ SearchViewInToolbar/app/src/main/java/github/nisrulz/example/searchviewintoolbar/MainActivity.kt
@@ -0,0 +1,36 @@
+package github.nisrulz.example.searchviewintoolbar
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import github.nisrulz.example.searchviewintoolbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.searchview_menu, menu)
+        val searchViewItem = menu.findItem(R.id.action_search)
+        val searchViewAndroidActionBar = searchViewItem.actionView as SearchView
+        searchViewAndroidActionBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchViewAndroidActionBar.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+}
